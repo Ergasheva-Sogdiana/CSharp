cf6890676b5abf0885fcaa5369c474d8dad14df5
Sogdiana <ergashevasogdianas@gmail.com>
2024-06-28 17:34:07 +0500

    Add Lesson2:Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//int n = 10;
+int[] array = {1, 4, 5, 7, 7, 35, 1, 3, 5 };
+int i = 0;
+
+while(i < array.Length)
+{
+    if (array[i] % 2 == 0 ){
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
