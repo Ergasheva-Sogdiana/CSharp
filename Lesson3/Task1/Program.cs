cc579e8347b732db02b75b7135ecb8acc2b23a1a
Sogdiana <ergashevasogdianas@gmail.com>
2024-07-20 12:10:24 +0500

    add Lesson3:task1

--- Lesson3/Task1/Program.cs
+++ Lesson3/Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+
+System.Console.WriteLine(result);
